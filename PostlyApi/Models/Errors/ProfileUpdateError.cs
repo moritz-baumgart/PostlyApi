b58886b014b8be50dac4835313e9da04e7a52394
Malin0ne <maja.koester@student.uni-siegen.de>
2023-05-30 00:51:53 +0200

    Add ProfileUpdateError

---
+++ PostlyApi/Models/Errors/ProfileUpdateError.cs
@@ -0,0 +1,10 @@
+﻿namespace PostlyApi.Models.Errors
+{
+    public enum ProfileUpdateError
+    {
+        None,
+        InvalidEmail,
+        InvalidPhoneNumber,
+        InvalidBirthday,
+    }
+}
