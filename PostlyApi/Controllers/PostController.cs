afdb479daf5b4968ce4b5e945c3a943ebb7667a2
Malin0ne <maja.koester@student.uni-siegen.de>
2023-06-10 17:57:46 +0200

    Add endpoint to remove vote from post
    > Seperate endpoint for removing vote instead of VoteInteractionType.Remove

--- PostlyApi/Controllers/PostController.cs
+++ PostlyApi/Controllers/PostController.cs
@@ -154,7 +154,7 @@ namespace PostlyApi.Controllers
         [HttpGet("{postId}/downvotes")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserDTO>))]
         [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Error))]
-        public ActionResult<IEnumerable<UserDTO>> Downvotes([FromRoute] long postId)
+        public ActionResult<IEnumerable<UserDTO>> GetDownvotes([FromRoute] long postId)
         {
             var post = _db.Posts
                 .Where(p => p.Id == postId)
@@ -183,14 +183,14 @@ namespace PostlyApi.Controllers
         /// Adds the specified reaction to the post with given post id.
         /// </summary>
         /// <param name="postId">The post id of the targeted post</param>
-        /// <param name="vote">The type of vote that should be applied</param>
-        /// <returns>The <see cref="PostDTO"/> of the targeted post </returns>
+        /// <param name="voteType">The type of vote that should be applied</param>
+        /// <returns>A <see cref="VoteUpdateViewModel"/></returns>
         [HttpPost("{postId}/vote")]
         [Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VoteUpdateViewModel))]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Error))]
-        public ActionResult SetVote([FromRoute] int postId, [FromBody] VoteInteractionType vote)
+        public ActionResult<VoteUpdateViewModel> SetVote([FromRoute] int postId, [FromBody] VoteType voteType)
         {
             var user = DbUtilities.GetUserFromContext(HttpContext, _db);
 
@@ -204,45 +204,84 @@ namespace PostlyApi.Controllers
 
             _db.Entry(post).Collection(p => p.Votes).Load();
 
-            var existingVote = post.Votes.Where(v => v.PostId == postId && v.UserId == user.Id).FirstOrDefault();
+            var existingVote = post.Votes
+                .Where(v => v.PostId == postId && v.UserId == user.Id)
+                .FirstOrDefault();
 
-            switch (vote)
+            // if there is no vote yet, add a new one
+            if (existingVote == null)
             {
-                case VoteInteractionType.Upvote:
-                    if (existingVote != null)
-                    {
-                        existingVote.VoteType = VoteType.Upvote;
-                        break;
-                    }
-                    post.Votes.Add(new Vote
-                    {
-                        User = user,
-                        VoteType = VoteType.Upvote,
-                    });
-                    break;
-                case VoteInteractionType.Downvote:
-                    if (existingVote != null)
-                    {
-                        existingVote.VoteType = VoteType.Downvote;
-                        break;
-                    }
-                    post.Votes.Add(new Vote
-                    {
-                        User = user,
-                        VoteType = VoteType.Downvote,
-                    });
-                    break;
-                case VoteInteractionType.Remove:
-                    if (existingVote == null) break;
-                    post.Votes.Remove(existingVote);
-                    break;
+                post.Votes.Add(new Vote
+                {
+                    User = user,
+                    VoteType = voteType
+                });
+            }
+            // if there is one, just set the type
+            else
+            {
+                existingVote.VoteType = voteType;
+            }
+
+            _db.SaveChanges();
+
+            var upvoteCount = post.Votes
+                .Where(p => p.VoteType == VoteType.Upvote)
+                .Count();
+
+            var downvoteCount = post.Votes
+                .Where(p => p.VoteType == VoteType.Downvote)
+                .Count();
+
+            var result = new VoteUpdateViewModel()
+            {
+                PostId = postId,
+                UserId = user.Id,
+                VoteType = voteType,
+                UpvoteCount = upvoteCount,
+                DownvoteCount = downvoteCount
+            };
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Removes the users vote from the post with given post id.
+        /// </summary>
+        /// <param name="postId">The post id of the targeted post</param>
+        /// <returns></returns>
+        [HttpDelete("{postId}/vote")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VoteUpdateViewModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Error))]
+        public ActionResult<VoteUpdateViewModel> RemoveVote([FromRoute] int postId)
+        {
+            var user = DbUtilities.GetUserFromContext(HttpContext, _db);
+
+            if (user == null) { return Unauthorized(); }
+
+            var post = _db.Posts
+                .Where(p => p.Id == postId)
+                .FirstOrDefault();
+
+            if (post == null) { return NotFound(Error.PostNotFound); }
+
+            _db.Entry(post).Collection(p => p.Votes).Load();
+
+            var existingVote = post.Votes
+                .Where(v => v.PostId == postId && v.UserId == user.Id)
+                .FirstOrDefault();
+
+            if (existingVote != null)
+            {
+                post.Votes.Remove(existingVote);
             }
 
             _db.SaveChanges();
 
             return Ok();
         }
-
 
         /// <summary>
         /// Retrieves the comments of a post with given post id.
---
+++ PostlyApi/Models/DTOs/VoteUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PostlyApi.Enums;
+
+namespace PostlyApi.Models.DTOs
+{
+    // returned by the server when updating a vote
+    public class VoteUpdateViewModel
+    {
+        public int PostId { get; set; } // the post that had a vote update
+        public int UpvoteCount { get; set; } // the upvote count after the vote update
+        public int DownvoteCount { get; set; } // the downvote count after the vote update
+        public long UserId { get; set; } // the user who updated their vote
+        public VoteType? VoteType { get; set; } // the type of vote the user set
+    }
+}
