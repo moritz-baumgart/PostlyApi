7c401274b46d2227f4700f3baa23f9373e3d66b7
Malin0ne <maja.koester@student.uni-siegen.de>
2023-06-27 16:31:55 +0200

    Add SearchController
    > add endpoint to search by username

---
+++ PostlyApi/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostlyApi.Models;
+using PostlyApi.Models.DTOs;
+using PostlyApi.Utilities;
+
+namespace PostlyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly PostlyContext _db;
+
+        public SearchController(PostlyContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserDTO>))]
+        public IEnumerable<UserDTO> GetUser([FromQuery] string username)
+        {
+            var result = _db.Users
+                .Where(_ => _.Username.StartsWith(username))
+                .OrderBy(_ => _.Username)
+                .Select(_ => DbUtilities.GetUserDTO(_));
+
+            return result;
+        }
+    }
+}
